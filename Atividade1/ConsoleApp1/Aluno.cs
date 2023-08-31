bbb7dc455e5f4728ba21cb68754be6d37cbca8c6
Claudinei José dos Santos Junior <112285858+ClaudineiJr98@users.noreply.github.com>
2023-08-31 15:11:05 -0300

    Atividade1
    Atividade1

---
+++ Atividade1/ConsoleApp1/Aluno.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Aluno:Pessoa
+    {
+        public Aluno(string nome, string ra) : base(nome, ra)
+        {
+        }
+        public bool DocumentoAluno()
+        {
+            return RA == "G345AG2";
+        }
+    }
+}
---
+++ Atividade1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ConsoleApp1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("*** TRABALHO DE PSOO ***");
+        Aluno aluno = new Aluno("Claudinei Santos", "G345AG2");
+
+        Console.WriteLine("Nome: " + aluno.Nome);
+        Console.WriteLine("RA: " + aluno.RA);
+
+        if (aluno.DocumentoAluno())
+        {
+            Console.WriteLine("RA Valido");
+        }
+        else
+        {
+            Console.WriteLine("RA Invalido");
+        }
+    }
+}
